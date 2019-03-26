384ea8632489f538a90b02be72f9f80d9e73e827
MichaelChung123 <michael.chung1994@gmail.com>
2019-03-26 16:50:24 -0700

    success: adding file to bucket from app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,72 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fileType = require('file-type');
+const bluebird = require('bluebird');
+const multiparty = require('multiparty');
+
+require('dotenv').config();
+
+// // configure the keys for accessing AWS
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
+
+AWS.config.setPromisesDependency(bluebird);
+
+const s3 = new AWS.S3();
+
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+}
+
+app.post('/test-upload', (request, response) => {
+  const form = new multiparty.Form();
+  form.parse(request, async (error, fields, files) => {
+    if (error) throw new Error(error);
+    try {
+      console.log("Posting to ", process.env.S3_BUCKET);
+
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const timestamp = Date.now().toString();
+      const fileName = `bucketFolder/${timestamp}-lg`;
+      const data = await uploadFile(buffer, fileName, type);
+
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
+});
+
+let bucketParams = {
+  Bucket: 'imageupload-media'
+};
+
+s3.listObjects(bucketParams, function(err, data) {
+  if(err) {
+    console.log("error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
+
+app.get('test-upload', (request, response) => {
+
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class ImageUploader extends Component {
 
@@ -7,26 +8,41 @@ class ImageUploader extends Component {
     super(props);
 
     this.state = {
-      selectedFile: null
+      file: null
     }
   }
 
-  fileSelectedHandler = (event) => {
-    console.log(event.target.files[0]);
-    // this.setState({
-    //   selectedFile: event.target.files[0]
-    // });
+  submitFile = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file[0]);
+    axios.post(`/test-upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      // handle your response;
+      console.log("Submit:", response);
+    }).catch(error => {
+      // handle your error
+      console.log(error);
+    });
+
+    // axios.get();
   }
 
-  fileUploadHandler = () => {
-
+  handleFileUpload = (event) => {
+    this.setState({ file: event.target.files });
   }
+
 
   render() {
     return (
       <div className="uploader">
-        <input type="file" onChange={this.fileSelectedHandler} />
-        <button onClick={this.fileUploadHandler}>Upload</button>
+        <form onSubmit={this.submitFile}>
+          <input type="file" label='upload file' onChange={this.handleFileUpload} />
+          <button type='submit'>Upload</button>
+        </form>
       </div>
     );
   }
